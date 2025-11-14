637f95b791d1037d69c91f9ca02affec304f3fa2
Rober Guerrero <roberguerreromendoza@gmail.com>
2025-11-14 01:36:15 -0400

    Standardize configs and optimize deployment processes
    Updated `docker-compose.yml` to include `restart: unless-stopped` for `milkingyield.api` and added `depends_on` for dependencies. Changed `milkingyield.db` to use `postgres:17` and updated environment variables and volume paths. Updated `cattlemanagement.db` to use `mcr.microsoft.com/mssql/server:2022-latest`.
    
    Simplified `ApplyMigrations` in `MigrationExtensions.cs` for both projects by removing exception handling and logging. Unified database connection strings to use a generic `Database` key in `appsettings.json`.
    
    Added a multi-stage `Dockerfile.original` for `MilkingYield.API` using .NET 9.0 images to optimize build and deployment. These changes improve maintainability, standardize configurations, and enhance deployment processes.

--- src/CattleManagement/CattleManagement.API/Extensions/MigrationExtensions.cs
+++ src/CattleManagement/CattleManagement.API/Extensions/MigrationExtensions.cs
@@ -6,31 +6,18 @@ namespace CattleManagement.API.Extensions;
 
 internal static class MigrationExtensions
 {
-    internal static WebApplication ApplyMigrations(this WebApplication app)
+    internal static void ApplyMigrations(this WebApplication app)
     {
-        using var scope = app.Services.CreateScope();
-        var services = scope.ServiceProvider;
-        try
-        {
-            AppDbContext context = services.GetRequiredService<AppDbContext>();
-            if (context.Database.GetPendingMigrations().Any())
-            {
-                context.Database.Migrate();
-            }
-        }
-        catch (Exception ex)
-        {
-            ILogger<Program> logger = services.GetRequiredService<ILogger<Program>>();
-            logger.LogError(ex, "An error occurred while migrating or seeding the database.");
-        }
-        return app;
+        using IServiceScope scope = app.Services.CreateScope();
+        AppDbContext context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        context.Database.Migrate();
     }
     internal static IServiceCollection AddDatabaseProvider(
         this IServiceCollection services,
         IConfiguration configuration)
     {
         services.AddSqlServer<AppDbContext>(
-            configuration.GetConnectionString("CattleDatabase"),
+            configuration.GetConnectionString("Database"),
             optionsAction: (options) => options.UseSeeding((context, _) =>
             {
                 context.Set<Cattle>().AddRange(SampleData.Cattles);
